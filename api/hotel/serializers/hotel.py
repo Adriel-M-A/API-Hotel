aad80b00fe5c2bf05a0e0b2c1c476a917043b6ed
Adriel-M-A <montes.adriel10@gmail.com>
2023-12-05 21:57:00 -0300

    Organizaciones de Serializadores

--- api/hotel/serializers/hotel.py
+++ api/hotel/serializers/hotel.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apps.hotel.models import Hotel, Habitacion, Paquete, Temporada, Descuento
+from apps.hotel.models import Hotel
 
 # Importar serializadores del core
 from api.core.serializers.ubicacion import UbicacionSerializer
@@ -28,60 +28,3 @@ class HotelFullSerializer(HotelMidSerializer):
 
     class Meta(HotelMidSerializer.Meta):
         fields = HotelMidSerializer.Meta.fields + ["vendedores"]
-
-
-# Cosas faltantes para hotel full
-# Todo lo que es por disponibilidad nose como hacerlo :P por todos los parametros que se tienen que enviar (minimo fechas inicio y fin)
-
-# Ver cuales paqutes del hotel mostrar (todos o solo disponibles)
-# Habitaciones disponibles, enviar una lista de habitaciones o organizarlas segun su tipo o organizarlas desde al front.
-
-
-class HabitacionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Habitacion
-        fields = "__all__"
-
-
-class PaqueteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Paquete
-        fields = "__all__"
-
-
-class TemporadaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Temporada
-        fields = "__all__"
-
-
-class DescuentoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Descuento
-        fields = "__all__"
-
-
-class DisponibilidadSerializer(serializers.Serializer):
-    habitaciones = serializers.PrimaryKeyRelatedField(
-        queryset=Habitacion.objects.all(), many=True
-    )
-    inicio = serializers.DateTimeField()
-    fin = serializers.DateTimeField()
-    # descuento
-    # paquete
-    # temporada
-
-    def validate(self, attrs):
-        attrs = super().validate(attrs)
-        habitaciones = attrs["habitaciones"]
-        print(habitaciones)
-        # raise ValidationError("No me gusta tu habitacion")
-        return attrs
-
-    def create(self, validated_data):
-        hotel = validated_data["hotel"]
-        habitaciones = validated_data["habitaciones"]
-        desde = validated_data["inicio"]
-        hasta = validated_data["fin"]
-        disponibilidad = hotel.verificar_disponibilidad(habitaciones, desde, hasta)
-        return disponibilidad
---
+++ api/hotel/serializers/otros.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from apps.hotel.models import Habitacion, Paquete, Temporada, Descuento
+
+class HabitacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habitacion
+        fields = "__all__"
+
+
+class PaqueteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paquete
+        fields = "__all__"
+
+
+class TemporadaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Temporada
+        fields = "__all__"
+
+
+class DescuentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Descuento
+        fields = "__all__"
